b1cb4595062f04756a0cc560f85277e409ca5c62
vpullakhandam <124179668+vpullakhandam@users.noreply.github.com>
2025-12-08 18:07:14 +0530

    updated deps

--- src/chunking.py
+++ src/chunking.py
@@ -1,6 +1,6 @@
 from typing import List
 from model.schema import Chunk
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 
 def chunking_markdown(markdown_text,page_map)->List[Chunk]:
